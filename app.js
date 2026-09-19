a1c5bc57df1d3d7ff1342286976e649ae1965f55
lankeshwar1 <hippalitmus@gmail.com>
2026-09-19 11:14:40 +0530

    App.js file changed

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("DevOps Project 5 application started");
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('DevOps Project 5 application is running');
+});
+
+server.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}`);
+});
